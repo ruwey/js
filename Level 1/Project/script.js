f0189de066a1ad3d87696e94257deae816f485b5
ruwey <gd@ruwey.com>
2022-12-14 22:00:58 -0500

    Finished Lvl 1 Project

--- Level 1/Project/script.js
+++ Level 1/Project/script.js
@@ -1,7 +1,11 @@
 function init() {
-    var time = 7;
-    var guesses = [];
-    var code = genCode();
+    time = 7;
+    timeDisplay = document.getElementById("time");
+    guesses = [];
+    guessesDisplay = document.getElementById("guessList");
+    code = genCode();
+    currentGuess = "";
+    currentGuessDisplay = document.getElementById("currentGuess");
     console.log("Code: ", code);
 
 }
@@ -14,3 +18,64 @@ function genCode() {
     return code;
 }
 
+function enterKey(key) {
+    currentGuess += key;
+
+    if (currentGuess.length == 3)
+	 tryCode(currentGuess);
+
+    display();
+}
+
+function clearKeypad() {
+    currentGuess = "";
+
+    display();
+}
+
+function tryCode(testCode) {
+    time--;
+    if (time == 0) {
+	resetGame();
+	guesses.splice(0, 0, "LOSS");
+	currentGuess = "";
+	return;
+    }
+
+    let relation;
+    if (code > testCode)
+	relation =  -1;
+    else if (code < testCode)
+	relation = 1;
+    else {
+	relation = 0;
+	// Reset on Correct
+	resetGame();
+    }
+
+    guesses.splice(0, 0, [testCode, relation]);
+    currentGuess = "";
+}
+
+function resetGame() {
+    code = genCode();
+    time = 7;
+}
+
+function display() {
+    timeDisplay.innerHTML = time;
+    currentGuessDisplay.innerHTML = currentGuess;
+
+    let guessList = "";
+    for (entry of guesses) {
+	if (entry == "LOSS")
+	    guessList += "You Lost!! New Game Begins";
+	else
+	    guessList += "Tried " + entry[0] + " which is " +
+	        (entry[1] == 0 ? "a success!" :
+		 entry[1] == 1 ? "too large" : "too small");
+	guessList += "<br/>";
+    }
+
+    guessesDisplay.innerHTML = guessList;
+}
